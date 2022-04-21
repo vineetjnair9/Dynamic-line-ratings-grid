e28533596f0fa285de6b10cc7640afb14ec71fac
jvineet9 <jvineet9@mit.edu>
2022-04-21 13:09:43 -0400

    added branch

--- solve_dlr_project.jl
+++ solve_dlr_project.jl
@@ -4,8 +4,9 @@
 using PowerModels, Gurobi
 using DataFrames, CSV
 
-
-result = run_dc_opf("D:/papers/texas_test.m",
+Path_Vineet = "/Users/vinee/ScenarioData/texas.m"
+# Path_Thomas = "D:/papers/texas_test.m" 
+result = run_dc_opf(Path_Vineet,
                     Gurobi.Optimizer,
                     setting = Dict("output" => Dict("duals" => true)))
 ;
@@ -15,16 +16,16 @@ d = result["solution"]["bus"]
 
 
 lmps = [[parse(Int,k), d[k]["lam_kcl_r"]] for k in keys(d)];
-lmps = DataFrame(transpose(hcat(lmps...)));
-CSV.write("D:/papers/lmps.csv", lmps)
-
+lmps = DataFrame(Tables.table(transpose(hcat(lmps...))));
+# CSV.write("D:/papers/lmps.csv", lmps)
+CSV.write("/Users/vinee/Library/CloudStorage/OneDrive-MassachusettsInstituteofTechnology/MIT/Semesters/Spring 2022/15.S08/Project/lmps.csv", lmps)
 
 g = result["solution"]["gen"]
 
 gens = [[parse(Int,k), g[k]["pg"]] for k in keys(g)];
-gens = DataFrame(transpose(hcat(gens...)));
-CSV.write("D:/papers/gens.csv", gens)
-
+gens = DataFrame(Tables.table(transpose(hcat(gens...))));
+# CSV.write("D:/papers/gens.csv", gens)
+CSV.write("/Users/vinee/Library/CloudStorage/OneDrive-MassachusettsInstituteofTechnology/MIT/Semesters/Spring 2022/15.S08/Project/gens.csv", gens)
 
 
 result["solution"]["branch"]
