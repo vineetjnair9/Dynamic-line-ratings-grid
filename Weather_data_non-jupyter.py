81f3ac42321547899d6f1c4029ca6d48c2365d36
Vineet J Nair <jvineet9@mit.edu>
2022-05-03 20:53:26 -0400

    got weather data

--- Weather_data_non-jupyter.py
+++ Weather_data_non-jupyter.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from metpy.units import units
 from netCDF4 import num2date
 import numpy as np
@@ -10,6 +11,22 @@ import pandas as pd
 from powersimdata.input.grid import Grid
 import pickle
 from urllib.error import HTTPError
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+def avail_hours(Y, M, D):
+    dt = datetime(Y, M, D)
+    catalog = 'https://www.ncei.noaa.gov/thredds/catalog/model-rap130anl-old/'
+    url = '{}{dt:%Y%m}/{dt:%Y%m%d}/catalog.html'.format(catalog, dt=dt)
+
+    reqs = requests.get(url)
+    soup = BeautifulSoup(reqs.text, 'html.parser')
+    hrefs = pd.Series([link.get('href') for link in soup.find_all('a')])
+    href_df = pd.DataFrame({k: hrefs[hrefs.str.contains('.grb')].str.split('_').str[k].str.split('.').str[0] for k in [-2, -1]})
+    hrs = href_df[href_df[-1]=='001'][-2].drop_duplicates().sort_values()
+    return list(hrs.str[:2].astype(int))
 
 # Helper function for finding proper time variable
 def find_time_var(var, time_basename='time'):
@@ -18,16 +35,13 @@ def find_time_var(var, time_basename='time'):
             return coord_name
     raise ValueError('No time variable found for ' + var.name)
 
-# times = pd.date_range(start='1/1/2016', end='1/1/2017',freq='H')
-times = pd.date_range(start='1/1/2016', end='1/2/2016',freq='H')
-num_periods = times.size
-
 # Empty dictionaries to store data at each timestep
-x_all = {}
-y_all = {}
 u_all = {}
 v_all = {}
 temp_all = {}
+x_orig_all = {}
+y_orig_all = {}
+times = {}
 
 base_url = 'https://www.ncei.noaa.gov/thredds/ncss/model-rap130anl-old/'
 
@@ -40,98 +54,105 @@ west_box = buses.lon.min() - 1
 east_box = buses.lon.max() + 1
 
 # Loop through all times
-for i in range(num_periods):
+# times = pd.date_range(start='1/1/2016', end='1/1/2017',freq='H')
 
-    # dt = datetime(2016, 1, 1, 12) # Y, M, D, H
-    dt = times[i]
+# Hour counter
+h = 0
 
-    try: 
+for date in pd.date_range(start='2016-2-2', end='2017-1-1', freq='D'):
+    print(date, end=' ')
+    hrs = avail_hours(date.year, date.month, date.day)
+    print(len(hrs))
+    for hr in hrs:
+        dt = pd.to_datetime('{}-{}-{} {}:00'.format(date.year, date.month, date.day, hr))
+    
         ncss = NCSS('{}{dt:%Y%m}/{dt:%Y%m%d}/rap_130_{dt:%Y%m%d}'
-                '_{dt:%H}00_000.grb2'.format(base_url, dt=dt))
-    except HTTPError: # Just skip this hour & go to the next if not available
-        continue
+                    '_{dt:%H}00_000.grb2'.format(base_url, dt=dt))
 
-    # Create lat/lon box for location you want to get data for
-    query = ncss.query().time(dt)
-    query.lonlat_box(north=north_box, south=south_box, east=east_box, west=west_box)
-    query.accept('netcdf')
+        # Create lat/lon box for location you want to get data for
+        query = ncss.query().time(dt)
+        query.lonlat_box(north=north_box, south=south_box, east=east_box, west=west_box)
+        query.accept('netcdf')
 
-    # Request data for model "surface" data
-    query.variables('Temperature_height_above_ground',
-                    'u-component_of_wind_height_above_ground',
-                    'v-component_of_wind_height_above_ground')
-    data = ncss.get_data(query)
+        # Request data for model "surface" data
+        query.variables('Temperature_height_above_ground',
+                        'u-component_of_wind_height_above_ground',
+                        'v-component_of_wind_height_above_ground')
+        data = ncss.get_data(query)
 
-    filterwarnings("ignore", category=DeprecationWarning) 
+        filterwarnings("ignore", category=DeprecationWarning) 
 
-    # Pull out variables you want to use
-    temp = units.K * data.variables['Temperature_height_above_ground'][:].squeeze()
-    lev_80m_temp = np.where(data.variables['height_above_ground1'][:] == 80)[0][0]
-    temp_80m = temp[lev_80m_temp]
-    u_wind = units('m/s') * data.variables['u-component_of_wind_height_above_ground'][:].squeeze()
-    v_wind = units('m/s') * data.variables['v-component_of_wind_height_above_ground'][:].squeeze()
-    x = data.variables['x'][:].squeeze()
-    y = data.variables['y'][:].squeeze()
-    time_var = data.variables[find_time_var(data.variables['Temperature_height_above_ground'])]
+        # Pull out variables you want to use
+        temp = units.K * data.variables['Temperature_height_above_ground'][:].squeeze()
+        lev_80m_temp = np.where(data.variables['height_above_ground1'][:] == 80)[0][0]
+        temp_80m = temp[lev_80m_temp]
+        u_wind = units('m/s') * data.variables['u-component_of_wind_height_above_ground'][:].squeeze()
+        v_wind = units('m/s') * data.variables['v-component_of_wind_height_above_ground'][:].squeeze()
+        x_orig = data.variables['x'][:].squeeze()
+        y_orig = data.variables['y'][:].squeeze()
+        time_var = data.variables[find_time_var(data.variables['Temperature_height_above_ground'])]
 
-    # Convert number of hours since the reference time into an actual date
-    time = num2date(time_var[:].squeeze(), time_var.units)
+        # Convert number of hours since the reference time into an actual date
+        time_val = num2date(time_var[:].squeeze(), time_var.units)
 
-    lev_80m_wind = np.where(data.variables['height_above_ground4'][:] == 80)[0][0]
-    u_wind_80m = u_wind[lev_80m_wind]
-    v_wind_80m = v_wind[lev_80m_wind]
+        lev_80m_wind = np.where(data.variables['height_above_ground4'][:] == 80)[0][0]
+        u_wind_80m = u_wind[lev_80m_wind]
+        v_wind_80m = v_wind[lev_80m_wind]
 
-    # Convert masked arrays to regular numpy arrays
-    x = x.compressed()
-    y = y.compressed()
+        # # Convert masked arrays to regular numpy arrays
+        # x = x_orig.compressed()
+        # y = x_orig.compressed()
 
-    # Combine 1D x and y coordinates into a 2D grid of locations
-    x_2d, y_2d = np.meshgrid(x, y,indexing='ij')
+        # # Combine 1D x and y coordinates into a 2D grid of locations
+        # x_2d, y_2d = np.meshgrid(x, y,indexing='ij')
 
-    # Get subset of valid points for which we have data
-    x_valid_temp = x_2d[~temp_80m.mask].ravel()
-    x_valid_u = x_2d[~u_wind_80m.mask].ravel()
-    x_valid_v = x_2d[~v_wind_80m.mask].ravel()
+        # # Get subset of valid points for which we have data
+        # x_valid_temp = x_2d[~temp_80m.mask].ravel()
+        # x_valid_u = x_2d[~u_wind_80m.mask].ravel()
+        # x_valid_v = x_2d[~v_wind_80m.mask].ravel()
 
-    y_valid_temp = y_2d[~temp_80m.mask].ravel()
-    y_valid_u = y_2d[~u_wind_80m.mask].ravel()
-    y_valid_v = y_2d[~v_wind_80m.mask].ravel()
+        # y_valid_temp = y_2d[~temp_80m.mask].ravel()
+        # y_valid_u = y_2d[~u_wind_80m.mask].ravel()
+        # y_valid_v = y_2d[~v_wind_80m.mask].ravel()
 
-    temp_80m_valid = temp_80m[~temp_80m.mask].ravel()
-    u_wind_valid = u_wind_80m[~u_wind_80m.mask].ravel()
-    v_wind_valid = v_wind_80m[~v_wind_80m.mask].ravel()
+        # temp_80m_valid = temp_80m[~temp_80m.mask].ravel()
+        # u_wind_valid = u_wind_80m[~u_wind_80m.mask].ravel()
+        # v_wind_valid = v_wind_80m[~v_wind_80m.mask].ravel()
 
-    points_temp = np.transpose(np.vstack((x_valid_temp,y_valid_temp)))
-    points_u = np.transpose(np.vstack((x_valid_u,y_valid_u)))
-    points_v = np.transpose(np.vstack((x_valid_v,y_valid_v)))
+        # points_temp = np.transpose(np.vstack((x_valid_temp,y_valid_temp)))
+        # points_u = np.transpose(np.vstack((x_valid_u,y_valid_u)))
+        # points_v = np.transpose(np.vstack((x_valid_v,y_valid_v)))
 
-    # Convert masked weather arrays to numpy arrays by interpolating to fill missing data points
-    # interpolation methods - nearest, linear, cubic
-    temp_80m_interp = griddata(points_temp, temp_80m_valid, (x_2d,y_2d), method='linear')
-    u_80m_interp = griddata(points_u, u_wind_valid, (x_2d,y_2d), method='linear')
-    v_80m_interp = griddata(points_v, v_wind_valid, (x_2d,y_2d), method='linear')
+        # # Convert masked weather arrays to numpy arrays by interpolating to fill missing data points
+        # # interpolation methods - nearest, linear, cubic
+        # temp_80m_interp = griddata(points_temp, temp_80m_valid, (x_2d,y_2d), method='linear')
+        # u_80m_interp = griddata(points_u, u_wind_valid, (x_2d,y_2d), method='linear')
+        # v_80m_interp = griddata(points_v, v_wind_valid, (x_2d,y_2d), method='linear')
 
-    # Convert x and y from lambert conformal conic projection to lat-lon
-    x_2d_array = x_2d.ravel()
-    y_2d_array = y_2d.ravel()
+        # # Convert x and y from lambert conformal conic projection to lat-lon
+        # x_2d_array = x_2d.ravel()
+        # y_2d_array = y_2d.ravel()
 
-    transformer = Transformer.from_crs('epsg:2154', 'epsg:4326')
-    lat,lon = transformer.transform(x_2d_array,y_2d_array)
+        # transformer = Transformer.from_crs('epsg:2154', 'epsg:4326')
+        # lat,lon = transformer.transform(x_2d_array,y_2d_array)
 
-    # Convert lat-lon to utm projection
-    result = utm.from_latlon(lat,lon)
-    x, y = result[0], result[1]
+        # # Convert lat-lon to utm projection
+        # result = utm.from_latlon(lat,lon)
+        # x, y = result[0], result[1]
 
-    x_all[i] = x
-    y_all[i] = y
-    temp_all[i] = temp_80m_interp
-    u_all[i] = u_80m_interp
-    v_all[i] = v_80m_interp
+        x_orig_all[h] = x_orig
+        y_orig_all[h] = y_orig
+        temp_all[h] = temp_80m
+        u_all[h] = u_wind_80m
+        v_all[h] = v_wind_80m
+        times[h] = dt
 
-with open(r"C:\Users\vinee\Documents\DLR-15s08-project\weather_data_2016.pkl", 'wb') as file:
-    pickle.dump(times, file)
-    pickle.dump(x_all, file)
-    pickle.dump(y_all, file)
+        h += 1
+        time.sleep(5)
+
+with open(r"C:\Users\vinee\OneDrive - Massachusetts Institute of Technology\MIT\Semesters\Spring 2022\15.S08\Project\weather_data_Feb-Dec2016.pkl", 'wb') as file:
+    pickle.dump(x_orig_all, file)
+    pickle.dump(y_orig_all, file)
     pickle.dump(temp_all, file)
     pickle.dump(u_all, file)
     pickle.dump(v_all, file)
